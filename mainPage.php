6be3a1d2694608d011645e14653b1cdcc7358b5f
ursy <nemeturska@gmail.com>
2016-03-24 14:27:53 +0100

    ..

--- mainPage.php
+++ mainPage.php
@@ -33,7 +33,7 @@
     <div id="website">
         <div>
             <ul class="nav">
-                <li style="float:left"><img class="banner" src="znamke.jpg"></img></li> <!--slika banner-->
+                <li style="float:left"><img class="banner" src="znamke.jpg"/></li> <!--slika banner-->
             </ul>
         </div>
 
@@ -107,7 +107,6 @@
 			<label id="cas_label">Čas oddaje: </label>
 			<input id="timestamp1" name="timestamp1" type="text" placeholder="Vnesi čas..." style="font-family: comforta;margin-top:25px;margin-left:130px;text-align:center;padding:10px;width:300px; height:20px;font-size:18px;border:1px solid #3DD9C9; background-color:#333; color: #d9d9d9" />
 			<button style="margin-top:35px;margin-left:235px;" type="button" id="naredi_zamenjavo" name="submit" class="btn">Naredi zamenjavo</button>
-			</select>
 		</div>
 	</div>
 
@@ -159,11 +158,11 @@
 
 	<!--urejanje uporabnikovega profila-->
 	<div id="edit_uporabnik_tabcontent" class="tabContent">
-		<div style="box-shadow: -5px 5px 5px -5px #333, 5px 5px 5px -5px #333;border-radius: 5px;margin-bottom:70px; margin-top:70px; margin-left:200px; width:400px; height:450px; background-color: #333;">
+		<div style="box-shadow: -5px 5px 5px -5px #333, 5px 5px 5px -5px #333;border-radius: 5px;margin-bottom:70px; margin-top:70px; margin-left:200px; width:400px; height:500px; background-color: #333;">
 			<h1>Uredi profil</h1>
 			<input type="text" placeholder="Ime*" style="font-family: comforta;margin-left:37px;text-align:center;padding:10px;width:300px; height:20px;font-size:18px;border:1px solid #3DD9C9; background-color:#333; color: #d9d9d9" id='ime_uporabnika'/>
 			<input type="text" placeholder="Priimek*" style="font-family: comforta;margin-top:25px;margin-left:37px;text-align:center;padding:10px;width:300px; height:20px;font-size:18px;border:1px solid #3DD9C9; background-color:#333; color: #d9d9d9" id='priimek_uporabnika'/>
-			<<input type="text" placeholder="Kraj" style="font-family: comforta;margin-top:25px;margin-left:50px;text-align:center;padding:10px;width:300px; height:20px;font-size:18px;border:1px solid #3DD9C9; background-color:#333; color: #d9d9d9" id='kraj_uporabnika'/>
+			<input type="text" placeholder="Kraj" style="font-family: comforta;margin-top:25px;margin-left:30px;text-align:center;padding:10px;width:300px; height:20px;font-size:18px;border:1px solid #3DD9C9; background-color:#333; color: #d9d9d9" id='kraj_uporabnika'/>
 			<input type="text" placeholder="E-mail" style="font-family: comforta;margin-top:25px;margin-left:37px;text-align:center;padding:10px;width:300px; height:20px;font-size:18px;border:1px solid #3DD9C9; background-color:#333; color: #d9d9d9" id='email_uporabnika'/>
 			<select id="spol_uporabnika">
 				<option value="male">Moški</option>
