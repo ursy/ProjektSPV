57314a3dbc3a9dc706045cbad2af66d8e00248c6
simona23 <simonasiljanovska23@gmail.com>
2016-04-12 20:22:56 +0200

    dodan dostop do chatov iz userjevega profila, manjka popravek za gumb nazaj

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ $(document).ready(function() {
 //prikaz podatkov o posamezni znamki
 $(document).on("mousedown", "tr.od_z", function() {
 	var id = $(this).attr("id");
-	//var id_znamka = $(this).attr("ID");
 	odpri_chat(userID, id, "", "");
 });
 
